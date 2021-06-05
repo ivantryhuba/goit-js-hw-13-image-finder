722ef577dd9a3918e9b279f4d29f17de4968d45c
Ivan Tryhuba <74875731+ivantryhuba@users.noreply.github.com>
2021-06-05 19:05:34 +0300

    checkpoint

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
-import './sass/main.scss';
+// import './sass/main.scss';
+// import { refs } from './js/refs'
+
+import PixApiService from './js/apiService';
+
+const refs = {
+  searchForm: document.querySelector('#search-form'),
+  gallery: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('[data-action="load-more"]'),
+};
+
+const pixApiService = new PixApiService();
+
+refs.searchForm.addEventListener('submit', onSearch);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
+
+
+
+function onSearch(evt) {
+    evt.preventDefault();
+
+    if (evt.currentTarget.elements.query.value.trim() === '') {
+        return
+    }
+
+    pixApiService.query = evt.currentTarget.elements.query.value.trim();
+    pixApiService.fetchArticles();
+}
+function onLoadMore() {
+  pixApiService.fetchArticles();
+    };
---
+++ src/js/apiService.js
@@ -0,0 +1,25 @@
+export default class PixApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+
+  fetchArticles(searchQuery) {
+    const API_KEY = '21947643-3ad5511e98ce1ab16d6eede2a';
+    const URL = `https://pixabay.com/api/?q=${this.searchQuery}&page=${this.page}&per_page=12&image_type=photo&orientation=horizontal&key=${API_KEY}`;
+
+    fetch(URL)
+      .then(r => r.json())
+      .then(data => {
+        this.page += 1;
+      });
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
